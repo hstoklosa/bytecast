794f3c1f07eb0ca026cd7b13cc8d6267c654b6da
hstoklosa <hubert.stoklosa23@gmail.com>
2025-02-28 01:28:27 +0000

    chore: migrate from scss to css

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,17 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { HlmButtonDirective } from "@spartan-ng/ui-button-helm";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [
-    RouterOutlet,
-    HlmButtonDirective
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, HlmButtonDirective],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'ByteCast';
+  title = "ByteCast";
 }
